0a0d80b643a3fd3c6cac87265facda2ce1c8bda6
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-02 04:55:19 +0000

    Add generate question function

--- brain.py
+++ brain.py
@@ -2,3 +2,9 @@ class QuizBrain:
     def __init__(self, q_list):
         self.num = 0
         self.list = q_list
+
+    def generate_question(self):
+        current = self.list[self.num]
+        self.num += 1
+        user = input(f'Q.{self.num} {current.text} ').title()
+        return user
